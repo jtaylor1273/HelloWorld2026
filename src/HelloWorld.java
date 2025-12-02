a518300d0e918247a88dbb276e51bb4e2478fdb8
johntaylor <jtaylor1273@outlook.com>
2025-12-02 17:16:03 -0500

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,6 @@
-import javax.swing.text.html.HTMLDocument;
 
 public class HelloWorld {
-    private String name = "";
+    private String name;
 
     public HelloWorld(String nameIn){
         this.name = nameIn;
@@ -15,10 +14,9 @@ public class HelloWorld {
     }
 
     public String toString(){
-        StringBuilder retVal = new StringBuilder();
-        retVal.append("Hello ");
-        retVal.append(this.name);
-        return retVal.toString();
+        String retVal = "Hello " +
+                this.name;
+        return retVal;
     }
 
 }
